f0c94d21941a87fc7e3cddf6042edc978d8070b7
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-01-09 14:18:04 +0530

    Update admins.py

--- Rax/utils/decorators/admins.py
+++ Rax/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from Rudra import app
-from Rudra.misc import SUDOERS, db
-from Rudra.utils.database import (
+from Rax import app
+from Rax.misc import SUDOERS, db
+from Rax.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
@@ -44,7 +44,7 @@ def AdminRightsCheck(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="RudramousAdmin",
+                            callback_data="RaxmousAdmin",
                         ),
                     ]
                 ]
@@ -140,7 +140,7 @@ def AdminActual(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="RudramousAdmin",
+                            callback_data="RaxmousAdmin",
                         ),
                     ]
                 ]
