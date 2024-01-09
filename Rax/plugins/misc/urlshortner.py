dab1cb625e66e03b2c6aa5e65aaa56de5c4c559b
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-01-09 14:36:46 +0530

    Update urlshortner.py

--- Rax/plugins/misc/urlshortner.py
+++ Rax/plugins/misc/urlshortner.py
@@ -3,7 +3,7 @@ from pyrogram import Client, enums, filters, idle
 import re
 from requests import get
 import asyncio
-from Rudra import app as app
+from Rax import app as app
 
 from pyrogram.types import InlineKeyboardButton as ikb, InlineKeyboardMarkup as ikm, Message
 from pyrogram.enums import ChatAction, ParseMode
@@ -82,7 +82,7 @@ async def unshort(bot, message):
 # app.add_handler(MessageHandler(unshort))
 __help__ = """
 ᴍᴀᴋᴇ sʜᴏʀᴛs ᴏғ ᴀ ɢɪᴠᴇɴ ʟɪɴᴋ 
- ❍ /short <url>  *:Example `/short https://t.me/Rudra_JAAT_1`.
+ ❍ /short <url>  *:Example `/short https://t.me/Rax_JAAT_1`.
  *"""
 
 __mod_name__ = "Sʜᴏʀᴛᴇɴᴇʀ"
