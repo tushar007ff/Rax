ad1f20d8e82856d58adb45a1379bec65f31ef4b9
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-01-09 14:29:02 +0530

    Update userbot.py

--- Rax/core/userbot.py
+++ Rax/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="RudraAss1",
+            name="RaxAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="RudraAss2",
+            name="RaxAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="RudraAss3",
+            name="RaxAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="RudraAss4",
+            name="RaxAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="RudraAss5",
+            name="RaxAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Rudra_JAAT_BIO")
+                await self.one.join_chat("Rax_JAAT_BIO")
               
-                await self.one.join_chat("Rudra_JAAT_BIO")
+                await self.one.join_chat("Rax_JAAT_BIO")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Rudra_JAAT_BIO")
+                await self.two.join_chat("Rax_JAAT_BIO")
                 
-                await self.one.join_chat("Rudra_JAAT_BIO")
+                await self.one.join_chat("Rax_JAAT_BIO")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Rudra_JAAT_BIO")
+                await self.three.join_chat("Rax_JAAT_BIO")
                 
-                await self.one.join_chat("Rudra_JAAT_BIO")
+                await self.one.join_chat("Rax_JAAT_BIO")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Rudra_JAAT_BIO")
+                await self.four.join_chat("Rax_JAAT_BIO")
                 
-                await self.one.join_chat("Rudra_JAAT_BIO")
+                await self.one.join_chat("Rax_JAAT_BIO")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Rudra_JAAT_BIO")
+                await self.five.join_chat("Rax_JAAT_BIO")
                 
-                await self.one.join_chat("Rudra_JAAT_BIO")
+                await self.one.join_chat("Rax_JAAT_BIO")
             except:
                 pass
             assistants.append(5)
